ff9a628f3240961410080a3575d70f5ba86e013e
Alex Obukhov <alexobukhov@gmail.com>
2023-03-11 00:16:23 +0300

    Домашнее задание 3

--- src/main/java/ru/gb/gbinterview/config/CommandLineRunnerImpl.java
+++ src/main/java/ru/gb/gbinterview/config/CommandLineRunnerImpl.java
@@ -2,32 +2,21 @@ package ru.gb.gbinterview.config;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-import ru.gb.gbinterview.model.Person;
-import ru.gb.gbinterview.model.figures.Round;
-import ru.gb.gbinterview.model.figures.Square;
-import ru.gb.gbinterview.model.figures.Triangle;
-
-import java.util.ArrayList;
-import java.util.LinkedList;
+import ru.gb.gbinterview.core.LockCounter;
+import ru.gb.gbinterview.core.PingPong;
 
 @Component
 public class CommandLineRunnerImpl implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
-        // Домашнее задание 1 Задача 1
-        String firstName = "FirstName";
-        Person person = new Person.Builder().addFirstName(firstName).build();
-        System.out.println(person.getFirstName());
+        // Домашнее задание 3 задача 1
+        PingPong pingPong = new PingPong();
+        for (int i = 0; i < 10; i++) {
+            pingPong.pingPong();
+        }
 
-        // Домашнее задание 1 задача 3
-        Round round = new Round("круг");
-        double diameter = round.getDiameter(15.63);
-        System.out.println(round.getName());
-
-        Square square = new Square("квадрат");
-        float squareSquare = square.getSquare(12.84F, 13.57F);
-
-        Triangle triangle = new Triangle("треугольник");
-        float angle = triangle.getAngle(40F, 120F);
+        // Домашнее задание 3 задача 2
+        LockCounter lockCounter = new LockCounter();
+        lockCounter.start();
     }
 }
